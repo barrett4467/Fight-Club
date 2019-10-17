178e0b4d53c1ee1ac07228ff66930ce1fc39ee05
Kevin Halstead <mrmichael423@icloud.com>
2019-10-17 13:37:09 -0600

    updated the server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
