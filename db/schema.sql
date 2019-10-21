5ba2b01d5ce7b09e01e10421a3a5fd012d726f6d
Kevin Halstead <mrmichael423@icloud.com>
2019-10-21 09:16:07 -0600

    need changes

--- db/schema.sql
+++ db/schema.sql
@@ -2,17 +2,4 @@ DROP DATABASE IF EXISTS fight_club;
 CREATE DATABASE fight_club;
 USE fight_club;
 
-CREATE TABLE players (
-	id INT AUTO_INCREMENT NOT NULL,
-	name VARCHAR(50),
-    hp INT,
-    attack INT,
-    PRIMARY KEY (id)
-);
-CREATE TABLE opponents (
-	id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(50),
-    hp INT,
-    attack INT,
-    PRIMARY KEY (id)
-);
+
