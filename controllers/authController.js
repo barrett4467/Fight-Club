f9d92f760e7394ebb87d588552b92cc89c6e4730
Kevin Halstead <mrmichael423@icloud.com>
2019-10-23 12:02:45 -0600

    fixed small bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,11 @@
 module.exports = {
+  signup: function(req, res) {
+    // if user is logged in, redirect to /
+    if (req.user) {
+      return res.redirect("/");
+    }
+    res.render("signup");
+  },
   login: function(req, res) {
     // if user is logged in, redirect to /dashboard
     if (req.user) {
@@ -8,11 +15,8 @@ module.exports = {
 
     res.render("login");
   },
-  signup: function(req, res) {
-    // if user is logged in, redirect to /
-    if (req.user) {
-      return res.redirect("/");
-    }
-    res.render("signup");
+  logout: function(req, res) {
+    req.logout();
+    res.redirect("/");
   }
 };
